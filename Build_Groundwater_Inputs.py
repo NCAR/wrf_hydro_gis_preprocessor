24bd2f0f450e8a15f4055f646704b254e2447c67
mcasali <50184471+mcasali@users.noreply.github.com>
2020-08-10 16:11:18 -0600

    Updated to run on command line

--- Build_Groundwater_Inputs.py
+++ Build_Groundwater_Inputs.py
@@ -13,24 +13,25 @@
 # Licence:     <your licence>
 # *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
 
-'''
-This script will build WRF-Hydro groundwater basin inputs using an input WPS Geogrid
-file and WRF-Hydro routing grid (Fulldom_hires.nc) file as inputs. Three methods
-are currently available for generating groundwater basins. One method, 'Polygon
-Shapefile or Feature Class' currently requires an input polygon shapefile defining
-the groundwater basins.
-'''
+descText = "This script will build WRF-Hydro groundwater basin inputs using an input WPS Geogrid " \
+           "file and WRF-Hydro routing grid (Fulldom_hires.nc) file as inputs. Three methods" \
+           "are currently available for generating groundwater basins. One method, 'Polygon" \
+           "Shapefile or Feature Class' currently requires an input polygon shapefile defining" \
+           "the groundwater basins."
 
 # Import Python Core Modules
 import os
 import time
 import shutil
+import sys
 import copy
 from distutils.version import LooseVersion
 
 # Import additional modules
 import netCDF4
 import gdal
+from argparse import ArgumentParser
+from pathlib import Path
 
 # Import function library into namespace. Must exist in same directory as this script.
 #import wrfhydro_functions as wrfh                                               # Function script packaged with this toolbox
@@ -43,29 +44,10 @@ from wrfhydro_functions import (WRF_Hydro_Grid, GW_nc, GWGRID_nc, dir_d8, stream
 
 # --- EDIT BELOW THIS LINE --- #
 
-# Input and output files and directories
-#inGeogrid = r'C:\Users\ksampson\Desktop\WRF_Hydro_GIS_Preprocessor_FOSS\geo_em.d01.nc'
-#inFulldom = r'C:\Users\ksampson\Desktop\WRF_Hydro_GIS_Preprocessor_FOSS\Outputs\TestGW\Fulldom_hires.nc'
-#out_dir = r'C:\Users\ksampson\Desktop\WRF_Hydro_GIS_Preprocessor_FOSS\Outputs\TestGW'
-
-inGeogrid = r'C:\Data\Projects\Arezoo\NewRiverInlet_V21\geo_em.d0x.nc'
-inFulldom = r'C:\Data\Projects\Arezoo\NewRiverInlet_V21\Fulldom_hires.nc'
-out_dir = r'C:\Data\Projects\Arezoo\NewRiverInlet_V21\Test_FOSS'
-
-# Outputs - permanent
-out_zip = os.path.join(out_dir, 'GroundwaterBasins_local.zip')
-
 # Provide the default groundwater basin generation method.
-# Options ['FullDom basn_msk variable', 'FullDom LINKID local basins', 'Polygon Shapefile or Feature Class']
-#defaultGWmethod = 'FullDom basn_msk variable'
-#defaultGWmethod = 'FullDom LINKID local basins'
-defaultGWmethod = 'Polygon Shapefile or Feature Class'
-
-# If the user selects 'Polygon Shapefile or Feature Class' as input, specify path here. Otherwise, in_GWPolys = None
-#in_GWPolys = None
-#in_GWPolys = r'C:\Users\ksampson\Desktop\WRF_Hydro_GIS_Preprocessor_FOSS\Inputs\GW_Basisn_Boundary_Unit.shp'
-#in_GWPolys = r'C:\Users\ksampson\Desktop\WRF_Hydro_GIS_Preprocessor_FOSS\Inputs\GW_Basisn_NHDPlus.shp'
-in_GWPolys = r'C:\Data\Projects\Arezoo\NewRiverInlet_V21\clipped_NHDPlus.shp'
+defaultGWmethod = 'FullDom basn_msk variable'
+defaultFulldom = 'Fulldom_hires.nc'
+defaultGeogrid = 'geo_em.d01.nc'
 
 # --- EDIT ABOVE THIS LINE --- #
 
@@ -85,8 +67,60 @@ if __name__ == '__main__':
     tic = time.time()
     print('Script initiated at {0}'.format(time.ctime()))
 
+    # Setup the input arguments
+    parser = ArgumentParser(description=descText, add_help=True)
+    parser.add_argument("-i",
+                        dest="in_nc",
+                        default='./{0}'.format(defaultGeogrid),
+                        help="Path to WPS geogrid (geo_em.d0*.nc) file or WRF-Hydro Fulldom_hires.nc file."
+                             " default=./geo_em.d01.nc")
+    parser.add_argument("-f",
+                        dest="in_fulldom",
+                        default='./{0}'.format(defaultFulldom),
+                        help="Path to WRF-Hydro Fulldom_hires.nc file. default=./Fulldom_hires.nc")
+    parser.add_argument("-m",
+                        dest="GWmethod",
+                        default='./{0}'.format(defaultFulldom),
+                        help="Method to create groundwater basins. Choose from 'FullDom basn_msk variable', "
+                             "'FullDom LINKID local basins', 'Polygon Shapefile or Feature Class'"
+                             " default=FullDom basn_msk variable")
+    parser.add_argument("-g",
+                        dest="in_GWPolys",
+                        default='',
+                        help="Path to groundwater basin polygon or feature class file.")
+    parser.add_argument("-o",
+                        dest="out_dir",
+                        default='',
+                        help="Output directory.")
+
+    # If no arguments are supplied, print help message
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    all_defaults = {key: parser.get_default(key) for key in vars(args)}
+
+    # Handle path of input
+    if args.in_nc == all_defaults["in_nc"]:
+        print('Using default input geogrid location of: {0}'.format(all_defaults["in_nc"]))
+        in_nc = Path.cwd().joinpath(defaultGeogrid)
+    else:
+        in_nc = args.in_nc
+
+    if args.in_fulldom == all_defaults["in_fulldom"]:
+        print('Using default input fulldom location of: {0}'.format(all_defaults["in_fulldom"]))
+
+    if args.GWmethod == all_defaults["GWmethod"]:
+        print('Using groundwater method of: {0}'.format(all_defaults["GWmethod"]))
+
+    if args.out_dir == all_defaults["out_dir"]:
+        print('Using default output location: {0}'.format(all_defaults["out_file"]))
+
+    # Outputs - permanent
+    out_zip = os.path.join(args.out_dir, 'GroundwaterBasins_local.zip')
+
     # Create scratch directory for temporary outputs
-    projdir = os.path.join(out_dir, 'gw_scratchdir')
+    projdir = os.path.join(args.out_dir, 'gw_scratchdir')
     projdir = os.path.abspath(projdir)
     if os.path.exists(projdir):
         shutil.rmtree(projdir)
@@ -101,8 +135,8 @@ if __name__ == '__main__':
     if saveBasins_Coarse:
         nclist.append(basinRaster)
 
-    rootgrp1 = netCDF4.Dataset(inGeogrid, 'r')
-    rootgrp2 = netCDF4.Dataset(inFulldom, 'r')
+    rootgrp1 = netCDF4.Dataset(args.in_nc, 'r')
+    rootgrp2 = netCDF4.Dataset(args.in_fulldom, 'r')
     if LooseVersion(netCDF4.__version__) > LooseVersion('1.4.0'):
         rootgrp2.set_auto_mask(False)                                            # Change masked arrays to old default (numpy arrays always returned)
     coarse_grid = WRF_Hydro_Grid(rootgrp1)                                 # Instantiate a grid object for the coarse grid
@@ -124,7 +158,7 @@ if __name__ == '__main__':
 
     # Build groundwater files
     print('  Building Groundwater Basin inputs.')
-    GWBasns = build_GW_Basin_Raster(inFulldom, projdir, defaultGWmethod, channelgrid, fdir, fine_grid, in_Polys=in_GWPolys)
+    GWBasns = build_GW_Basin_Raster(args.in_fulldom, projdir, defaultGWmethod, channelgrid, fdir, fine_grid, in_Polys=args.in_GWPolys)
     build_GW_buckets(projdir, GWBasns, coarse_grid, Grid=True, saveRaster=saveBasins_Coarse)
     if saveBasins_Fine:
         out_ds3 = gdal.GetDriverByName(RasterDriver).CreateCopy(basinRaster_Fine, GWBasns)
