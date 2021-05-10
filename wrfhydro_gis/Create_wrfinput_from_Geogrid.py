8713eb02676b0e4587cef4b929369131d2ec2bbd
Kevin <kmsampson@users.noreply.github.com>
2021-05-10 08:07:29 -0600

    Update Create_wrfinput_from_Geogrid.py
    Added capabilities provided by the xarray library. Now two methods exist (temporarily) to generate the wrfinput file: one using netCDF4-python and one using xarray. They provide identical outputs, except for the output file size due to compression options.

--- wrfhydro_gis/Create_wrfinput_from_Geogrid.py
+++ wrfhydro_gis/Create_wrfinput_from_Geogrid.py
@@ -35,7 +35,6 @@ import time
 from argparse import ArgumentParser
 
 # Import additional Python Modules
-import netCDF4
 import numpy
 
 # Module configurations
@@ -90,6 +89,7 @@ wedim = 'west_east'                                                             
 sndim = 'south_north'                                                           # Y-dimension name
 timedim = 'Time'                                                                # Time dimension name
 soildim = 'soil_layers_stag'                                                    # Soil layer dimension name
+soildimsize = 4
 keepDims = [timedim, 'month', sndim, wedim, soildim]                            # Dimensions to transfer from GEOGRID to WRFINPUT
 
 # Alter the names of variable names from GEOGRID (key) to WRFINPUT (value) variable names
@@ -117,6 +117,16 @@ addVars = [('TMN', 'K', [timedim, sndim, wedim], missFloat, 'f4'),
             ('ZS', 'm', [timedim, soildim], missFloat, 'f4'),
             ('DZS', 'm', [timedim, soildim], missFloat, 'f4')]
 
+# Choose the processing method for producing the output. Options = ['netcdf4-python', 'xarray']
+method = 'xarray'
+if method == 'netcdf4-python':
+    import netCDF4
+elif method == 'xarray':
+    import xarray as xr
+
+# Switch for fixing SoilT values of 0 over water areas
+fix_zero_over_water = True
+
 # --- Functions --- #
 def is_valid_file(parser, arg):
     # https://stackoverflow.com/questions/11540854/file-as-command-line-argument-for-argparse-error-message-if-argument-is-not-va
@@ -125,7 +135,7 @@ def is_valid_file(parser, arg):
     else:
         return str(arg)
 
-def fill_wrfinput(rootgrp_in, rootgrp_out, laimo=8):
+def fill_wrfinput_ncdfpy(rootgrp_in, rootgrp_out, laimo=8):
     '''
     This function will populate the arrays in the WRFINPUT file based on array and
     attribute values in the input GEOGRID file.
@@ -145,11 +155,12 @@ def fill_wrfinput(rootgrp_in, rootgrp_out, laimo=8):
 
     # SOILTEMP will show 0 value over water. This can cause issues when varying land cover fields
     # from default. Setting to mean non-zero values for now to have something reasonable.
-    soilT_mask = soilT<100                                                      # Create a mask of 'invalid' soil temperature values. All values <100K on SOILTEMP grid
-    soilT_mask_Mean = soilT[~soilT_mask].mean()                                 # Calculate the non-NAN soil temperature mean
-    soilT[soilT_mask] = soilT_mask_Mean                                         # Replace masked values with the mean of the unmasked values
-    if soilT_mask.sum()>0:
-        print('    Replaced {0} values in TMN with mean SOILTEMPT value ({1}).'.format(soilT_mask.sum(), soilT_mask_Mean))
+    if fix_zero_over_water:
+        soilT_mask = soilT<100                                                      # Create a mask of 'invalid' soil temperature values. All values <100K on SOILTEMP grid
+        soilT_mask_Mean = soilT[~soilT_mask].mean()                                 # Calculate the non-NAN soil temperature mean
+        soilT[soilT_mask] = soilT_mask_Mean                                         # Replace masked values with the mean of the unmasked values
+        if soilT_mask.sum()>0:
+            print('    Replaced {0} values in TMN with mean SOILTEMPT value ({1}).'.format(soilT_mask.sum(), soilT_mask_Mean))
 
     # TMN topographic adjustment
     print('    Performing topographic soil temperature adjustment.')
@@ -230,7 +241,7 @@ def main_ncdfpy(geoFile, wrfinFile, lai=8, outNCType='NETCDF4'):
     for dimname, dim in rootgrp_in.dimensions.items():
         if dimname in keepDims:
             rootgrp_out.createDimension(dimname, len(dim))                      # Copy dimensions from the GEOGRID file
-    soildimension = rootgrp_out.createDimension(soildim, 4)                     # Add soil_layers_stag dimension
+    soildimension = rootgrp_out.createDimension(soildim, soildimsize)           # Add soil_layers_stag dimension
 
     # Populate initial file with variables to keep from the input GEOGRID file
     for varname, ncvar in rootgrp_in.variables.items():
@@ -262,12 +273,153 @@ def main_ncdfpy(geoFile, wrfinFile, lai=8, outNCType='NETCDF4'):
             var[:] = ncvar[:]                                                   # Copy the variable data into the newly created variable
 
     # Process and populate variables
-    rootgrp_in, rootgrp_out = fill_wrfinput(rootgrp_in, rootgrp_out, laimo=lai)
+    rootgrp_in, rootgrp_out = fill_wrfinput_ncdfpy(rootgrp_in, rootgrp_out, laimo=lai)
 
     # Close and return
     rootgrp_in.close()
     rootgrp_out.close()
     return
+
+def fill_wrfinput_xarray(ds_in, laimo=8):
+    '''
+    This function will populate the arrays in the WRFINPUT file based on array and
+    attribute values in the input GEOGRID file.
+    '''
+
+    # Define variables using global attributes in GEOGRID file
+    iswater = ds_in.attrs.get('ISWATER')                                        # GEOGRID global attribute describing the LU_INDEX value assigned to water
+    isoilwater = ds_in.attrs.get('ISOILWATER')                                  # GEOGRID global describing the SOIL class assigned to water
+
+    # SOILTEMP will show 0 value over water. This can cause issues when varying land cover fields
+    # from default. Setting to mean non-zero values for now to have something reasonable.
+    hgt = ds_in['HGT'].data                                                     # Elevation in meters on the mass grid
+    if fix_zero_over_water:
+        soilT = ds_in['SOILTEMP'].data.copy()                                       # Soil temperature in degrees Kelvin
+        soilT_mask = soilT<100                                                      # Create a mask of 'invalid' soil temperature values. All values <100K on SOILTEMP grid
+        soilT_mask_Mean = soilT[~soilT_mask].mean()                                 # Calculate the non-NAN soil temperature mean
+        soilT[soilT_mask] = soilT_mask_Mean                                         # Replace masked values with the mean of the unmasked values
+        if soilT_mask.sum()>0:
+            print('    Replaced {0} values in TMN with mean SOILTEMPT value ({1}).'.format(soilT_mask.sum(), soilT_mask_Mean))
+
+    # TMN topographic adjustment
+    print('    Performing topographic soil temperature adjustment.')
+    tmn = soilT - 0.0065 * hgt                                                  # Topographic soil temperature adjustment
+    del soilT, hgt, soilT_mask, soilT_mask_Mean
+
+    # Create land/water mask from LU_INDEX in input GEOGRID file
+    use = ds_in['IVGTYP'].data                                                  # Landcover class
+    msk = use.copy()                                                            # Land use (LU_INDEX from GEOGRID)
+    msk[numpy.logical_and(msk>=0, msk!=iswater)] = 1                            # Set non-water cells to 1
+    msk[msk==iswater] = 2                                                       # Set all other cells to 2
+
+    # Find the dominant SOILCTOP value. If LU_INDEX is water, this will be water,
+    # otherwise it will be the dominant fractional non-water SOILCTOP category
+    soil_top_cat = ds_in['SOILCTOP'].data                                       # Soil texture class of the top soil layer
+    a = numpy.ma.array(soil_top_cat[0], mask=False)                             # Build a masked array in order to mask the water cateogry
+    a.mask[isoilwater-1] = True                                                 # Use a mask to mask the index of the water category
+    dominant_value = numpy.amax(a, axis=0).data                                 # Dominant soil category fraction (unmasked)
+    dominant_index = numpy.argmax(a, axis=0)+1                                  # Dominant soil category index (1-based index)
+    dominant_index[numpy.logical_and(dominant_value < 0.01, msk[0]==1)] = dom_lc_fill     # Set any land pixel values with tiny dominant fractions to a fill value
+    if numpy.logical_and(dominant_value < 0.01, msk[0]==1).sum() > 0:
+        print('    Replaced {0} values in ISLTYP with {1} because no dominant land class could be determined'.format(numpy.logical_and(dominant_value < 0.01, msk[0]==1).sum(), dom_lc_fill))
+    dominant_index[msk[0]==2] = isoilwater                                      # Set any values with landmask == 2 (water) to water
+    del a, dominant_value, soil_top_cat
+
+    # Set soils to the dominant type (derived above) and handle water/land soiltype mismatches
+    soi = dominant_index[numpy.newaxis]                                         # Add an axis to make this 3D (time, south_north, west_east)
+    soi[use==iswater] = isoilwater                                              # Make all water LU_INDEX cells into water soiltypes
+    soi[numpy.logical_and(use!=iswater, soi==isoilwater)] = fillsoiltyp         # If the pixel LU_INDEX is land type and SOILCTOP is water type, then make it water soil type
+    if numpy.logical_and(use!=iswater, soi==isoilwater).sum() > 0:
+        print('    Replaced {0} values in ISLTYP with {1} because of a land landcover type and water soil class'.format(fillsoiltyp, numpy.logical_and(use!=iswater, soi==isoilwater).sum()))
+    del dominant_index, use
+
+    # Soil moisture SMOIS 3D array
+    smoisArr = numpy.array([0.20, 0.21, 0.25, 0.27])                            # Constant soil moisture with increasing depth by vertical level
+    smois = smoisArr[:, None, None] * numpy.ones(msk.shape)                     # Set the soil moisture (SMOIS) array across entire domain by vertical level
+
+    # TSLB 3D array
+    tslbArr = numpy.array([285.0, 283.0, 279.0, 277.0])                         # Constant tslb with increasing depth by vertical level
+    tslb = tslbArr[:, None, None] * numpy.ones(msk.shape)                       # Set the TSLB array across entire domain by vertical level. tslb = numpy.vstack([msk]*4)
+
+    # Calculate the depths of the center depth of each soil layer based on the
+    # layer thicknesses provided in the header. Default is zs = [0.05, 0.25, 0.7, 1.5]
+    zs = [item/2 + sum(dzs[:num]) for num,item in enumerate(dzs)]               # Each center depth is half the layer thickness + sum of thicknesses of all levels above
+
+    veg = ds_in['GREENFRAC'].data * 100.0                               # Green fraction as a percentage (0-100)
+
+    # Populate output WRFINPUT file variable arrays
+    ds_in.variables['TMN'][:] = tmn                                       # Elevation adjusted deep soil temperature
+    ds_in.variables['XLAND'][:] = msk                                     # Landmask (1=land, 2=water) from LU_INDEX
+    ds_in.variables['SEAICE'][:] = numpy.zeros(msk.shape)                 # zeros
+    ds_in.variables['ISLTYP'][:] = soi                                    # Dominant soil type
+    ds_in.variables['SHDMAX'][:] = veg.max(axis=1)                        # Maximum GREENFRAC over time dimesion
+    ds_in.variables['SHDMIN'][:] = veg.min(axis=1)                        # Minimum GREENFRAC over time dimesion
+    ds_in.variables['LAI'][:] = ds_in.variables['LAI12M'][:,laimo-1] # Leaf area index for the user-specified month
+    ds_in.variables['CANWAT'][:] = numpy.zeros(msk.shape)                 # zeros
+    ds_in.variables['SNOW'][:] = numpy.zeros(msk.shape)                   # zeros
+    ds_in.variables['TSK'][:] = numpy.zeros(msk.shape) + 290.0            # Constant value
+    ds_in.variables['SMOIS'][:] = smois[numpy.newaxis]                    # Add an axis to make this 4D (time, soil_layer_stag, south_north, west_east)
+    ds_in.variables['TSLB'][:] = tslb[numpy.newaxis]                      # Add an axis to make this 4D (time, soil_layer_stag, south_north, west_east)
+    ds_in.variables['ZS'][:] = numpy.array(zs)[numpy.newaxis]             # Depths of the center of each soil layer
+    ds_in.variables['DZS'][:] = numpy.array(dzs)[numpy.newaxis]           # Thickness of each soil layer
+    del msk, veg, iswater, isoilwater, soi, smois, smoisArr, tslb, tslbArr, tmn, zs
+    return ds_in
+
+def main_xarray(geoFile, wrfinFile, lai=8, outNCType='NETCDF4'):
+    '''
+    This function is designed to build the wrfinput file using the xarray library.
+    '''
+    tic1 = time.time()
+
+    # --- Create initial file --- #
+    print('  Creating wrfinput file from geogrid file.')
+    print('    Input geogrid file: {0}'.format(geoFile))
+    print('    Output wrfinput file: {0}'.format(wrfinFile))
+    print('    Month selected (1=Januaray, 12=December): {0}'.format(lai))
+
+    # Open input files for reading. Lazy loading
+    ncDS = xr.open_dataset(geoFile)                         # , decode_cf=False
+
+    # Rename variables
+    ncDS = ncDS.rename(mapVars)
+
+    # Add new variables based on the addVars list
+    dims = dict(ncDS.dims)
+    dims.update({soildim:soildimsize})
+    newVars = []
+    for (varname, units, varDims, missing_value, dtype) in addVars:
+        da = xr.DataArray(data=numpy.empty(tuple([dims[dim] for dim in varDims]), dtype=dtype),
+            dims=varDims,
+            attrs={'units':units, 'missing_value':missing_value})
+        ncDS[varname] = da
+        newVars.append(varname)
+
+    # Process and populate variables
+    ncDS = fill_wrfinput_xarray(ncDS, laimo=lai)
+
+    # Drop dimensions
+    dropDims = [item for item in ncDS.dims if item not in keepDims]
+    ncDS = ncDS.drop_dims(dropDims)
+
+    # Drop variables first
+    keepVars2 = [mapVars.get(item,item) for item in keepVars] + newVars
+    dropVars = [item for item in ncDS.variables if item not in keepVars2]
+    ncDS = ncDS.drop(dropVars)
+
+    # Add global attributes
+    ncDS.attrs['Source_Software'] = 'WRF-Hydro {0} script (Python).'.format(sys.argv[0])
+    ncDS.attrs['creation_time'] = 'Created {0}'.format(time.ctime())
+
+    # Output file to disk
+    #encoding = {varname:ncDS[varname].encoding for varname in list(ncDS.variables.keys())}
+    encoding = {varname:{'_FillValue':None} for varname in list(ncDS.variables.keys())}
+    #for key, val in encoding.items():
+    #    val['_FillValue'] = None
+    ncDS.to_netcdf(wrfinFile, mode='w', format=outNCType, encoding=encoding)
+    ncDS.close()
+    del encoding, ncDS
+    return
+
 # --- End Functions --- #
 
 if __name__ == '__main__':
@@ -306,5 +458,8 @@ if __name__ == '__main__':
     args.in_Geogrid = os.path.abspath(args.in_Geogrid)
     args.out_wrfinput = os.path.abspath(args.out_wrfinput)
 
-    main_ncdfpy(args.in_Geogrid, args.out_wrfinput, lai=args.LAI_month, outNCType=outNCType)
+    if method == 'netcdf4-python':
+        main_ncdfpy(args.in_Geogrid, args.out_wrfinput, lai=args.LAI_month, outNCType=outNCType)
+    elif method == 'xarray':
+        main_xarray(args.in_Geogrid, args.out_wrfinput, lai=args.LAI_month, outNCType=outNCType)
     print('  Process completed in {0:3.2f} seconds'.format(time.time()-tic))
